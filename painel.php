58e28d905028527436819b1dcd547b093231fac8
guxtavobandeira <111713549+guxtavobandeira@users.noreply.github.com>
2024-02-23 23:10:16 -0300

    Create painel.php

---
+++ painel.php
@@ -0,0 +1,21 @@
+<?php
+
+include('protect.php');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+</head>
+<body>
+    Bem vindo ao painel,  <?php echo $_SESSION['nome']; ?>.
+    <p>
+        <a href="logout.php">Sair</a>
+    </p>
+</body>
+</html>
