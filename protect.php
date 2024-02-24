63c7e73e307ca2c2e8b2e2a0340dec2c859b9e7b
guxtavobandeira <111713549+guxtavobandeira@users.noreply.github.com>
2024-02-23 23:11:02 -0300

    Create protect.php

---
+++ protect.php
@@ -0,0 +1,10 @@
+<?php
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+    die("vc n pode acessar esta pagina, pq n esta logado.<p><a href=\"index.php\">Entrar</a></p>");
+}
+
