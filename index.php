e0ffcbd70502bf54c7050a1bddcf3d90a6e05004
guxtavobandeira <111713549+guxtavobandeira@users.noreply.github.com>
2024-02-24 19:27:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,33 @@
 <?php
-include('conexao.php');
 
-if(isset($_POST['email']) || isset($_POST['senha'])) {
+if(isset($_POST['email'])) {
     
-    if(strlen($_POST['email']) == 0) {
-    echo "preencha seu e-mail";
-    } else if(strlen($_POST['senha']) == 0) {
-        echo "preencha sua senha";
-    } else {
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
+    include('conexao.php');
 
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha ='$senha'";
-        $sql_querry = $mysqli->query($sql_code) or die("falha na execução do código sql" . $mysqli->error);
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
-        $quantidade = $sql_querry->num_rows;
-
-        if($quantidade == 1) {
-            $usuario = $sql_querry->fetch_assoc();
-
-            if(!isset($SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: painel.php");
-
-
-        } else{
-            echo "falha ao logar. email ou senha incorretos!";
-        }
-
-    }
+    $mysqli->query("INSERT INTO usuarios (nome, email, senha) VALUES('$nome','$email', '$senha')");
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <h1>Acesse sua conta:</h1>
-    <form action="" method="POST">
-        <p>
-            <label for="">E-mail</label>
-            <input type="text" name="email">
-        </p>
-        <p>
-            <label for="">Senha</label>
-            <input type="password" name="senha">
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
+    Cadastrar Senha
+    <form action="" method="post">
+        <input type="text" name="nome">
+        <input type="text" name="email">
+        <input type="text" name="senha">
+        <button type="submit">Cadastrar senha</button>
+        <br>
+        Já tem uma conta? <br>
+        acesse <a href="login.php">Acessar</a>
     </form>
-
 </body>
 </html>
