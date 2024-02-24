9de2e0f656cb16934d96c621217132f5eda59515
guxtavobandeira <111713549+guxtavobandeira@users.noreply.github.com>
2024-02-24 19:28:52 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,11 +1,13 @@
 <?php
 
-    $usuario = 'root';
-    $senha = '';
-    $database = 'ev_fisica';   
-    $host = 'localhost';
-    
-    $mysqli = new mysqli($host, $usuario, $senha, $database);
-    if ($mysqli->connect_error) {
-        die("falha ao conectar ao banco de dados" . $mysqli->error);
-    }
+$host = "localhost";
+$user = "root";
+$pass= "";
+$bd = "ev_fisica";
+
+$mysqli = new mysqli($host, $user, $pass, $bd);
+
+if ($mysqli->connect_errno) {
+    echo "conexao deu errado" . $mysqli->connect_error;
+    exit();
+}
