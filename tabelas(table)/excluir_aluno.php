2c166fb91f381cf235bb755dc776fff04d8e7d88
Gustavo Bandeira <111713549+guxtavobandeira@users.noreply.github.com>
2024-04-29 13:58:41 -0300

    Update excluir_aluno.php

--- tabelas(table)/excluir_aluno.php
+++ tabelas(table)/excluir_aluno.php
@@ -33,16 +33,45 @@ if (!empty($_GET['id_aluno']) && isset($_POST['confirmacao'])) {
 <html>
 <head>
     <title>Confirmação de Exclusão</title>
+    <!-- Adicione os arquivos de estilo e script do Bootstrap -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h2>Você realmente deseja excluir o aluno?</h2>
-    <form method="post">
-        <input type="hidden" name="confirmacao" value="sim">
-        <button type="submit">Sim</button>
-    </form>
-    <form action="tabelas.php" method="get">
-        <button type="submit">Cancelar</button>
-    </form>
+    <div class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Confirmação de Exclusão</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Você realmente deseja excluir o aluno?</p>
+                </div>
+                <div class="modal-footer">
+                    <form method="post">
+                        <input type="hidden" name="confirmacao" value="sim">
+                        <button type="submit" class="btn btn-danger">Sim</button>
+                    </form>
+                    <!-- Adicione o link para voltar para a página de tabelas -->
+                    <a href="tabelas.php" class="btn btn-secondary">Cancelar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Adicione os arquivos de script do Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <!-- Ative o modal automaticamente -->
+    <script>
+        $(document).ready(function() {
+            $('.modal').modal('show');
+        });
+    </script>
 </body>
 </html>
 <?php
